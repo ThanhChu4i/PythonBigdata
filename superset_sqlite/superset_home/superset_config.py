d0af206396ff905ce1ac7cfe8b4aa9545c170448
Thanhchu4i <chutienthanh444@gmail.com>
2024-12-05 09:41:28 +0700

    change

--- superset_sqlite/superset_home/superset_config.py
+++
@@ -1,3 +0,0 @@
-SQLALCHEMY_DATABASE_URI = 'postgresql+psycopg2://superset_user:superset_password@postgres:5432/superset_db'
-SQLALCHEMY_TRACK_MODIFICATIONS = False
-SECRET_KEY = "zQ+E6kxsQUK9xldHo9xz1U5YPJqa1R2j9XZlp8NfMv2HoH3oT8dNKD1GHM2QK/So"
