fc284ffce93450baa170c1c15d6cabf5f7f5680e
Thanhchu4i <chutienthanh444@gmail.com>
2024-12-06 13:21:51 +0700

    update

---
+++ superset_sqlite/superset_config.py
@@ -0,0 +1,7 @@
+# superset_config.py
+
+# Bật tính năng cho phép upload file
+FEATURE_FLAGS = {
+    "ALLOWED_FILE_UPLOADS": True,
+}
+DATA_UPLOAD_FOLDER = r'C:\Users\chuti\Downloads\processed_spotify_tracks_part-00000-75d557b4-f63b-4bd2-b6ab-3028b8df00e1-c000.csv'
